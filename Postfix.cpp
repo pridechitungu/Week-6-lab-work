464181c404a872b603ea81ebf257f5ab77e38746
pridechitungu <bsc-ele-19-24@unima.ac.mw>
2026-04-06 16:43:34 +0200

    postfix program

---
+++ Postfix.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+     
+    int x = 3;
+
+    //the value of x is inreased after the initial value of x is assigned to y 
+    //hence the value assigned to y is the value of x before it was inceased
+
+    int y = x++;
+    cout << "x: " << x << endl; //x will be 4
+    cout << "y: " << y << endl; //y will be 3
+
+    return 0;
+}
